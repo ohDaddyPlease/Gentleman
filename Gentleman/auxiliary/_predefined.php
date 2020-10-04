2f7f6a3bc93a1dd9ddf7f34ffd71f67f98313617
Serj <kraspvp@mail.ru>
2020-10-05 00:08:21 +0700

    .

--- Gentleman/auxiliary/_predefined.php
+++ Gentleman/auxiliary/_predefined.php
@@ -5,3 +5,11 @@ define('ROOT_PATH', realpath(dirname(__DIR__ . '/../../../')));
 define('APP_PATH', realpath(dirname(ROOT_PATH . '/app')));
 
 define('GENTLEMAN_PATH', realpath(dirname(ROOT_PATH . '/Gentleman')));
+
+define('CONFIGURATION_LOG_PATH',
+    ROOT_PATH . '/logs/configuration.log'
+);
+
+define('ENV_FILE_NAME', ROOT_PATH . '/.env');
+
+define('APP_LOG_PATH', ROOT_PATH . '/logs/' . APP_NAME . '.log');
